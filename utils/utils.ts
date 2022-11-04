bf7b466071607f8d1b091715c29244c1b9a21508
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-11-04 12:46:45 +0800

    Create utils.ts

---
+++ utils/utils.ts
@@ -0,0 +1,9 @@
+/**
+ * Transforms a string into an upper case string
+ *
+ * @param str String to be transformed
+ * @returns Transformed string
+ */
+export function strToUpper(str: string): string {
+  return str.toUpperCase();
+}
